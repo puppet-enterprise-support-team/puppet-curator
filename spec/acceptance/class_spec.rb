6d9915e0667b64040cc69c7fcd718f2200bca1cd
Stephan Eicher <stephan.eicher@check24.de>
2020-10-24 17:01:24 +0200

    Fix codestyle to comply with pdk standard

--- spec/acceptance/class_spec.rb
+++ spec/acceptance/class_spec.rb
@@ -5,8 +5,8 @@ describe 'curator class' do
     if fact('osfamily') == 'RedHat'
       it 'adds epel to and installs pip' do
         pp = "class { 'epel': }"
-        apply_manifest(pp, :catch_failures => true)
-        shell("yum -y install python-pip")
+        apply_manifest(pp, catch_failures: true)
+        shell('yum -y install python-pip')
         # Hack for EL7-based machines -- https://tickets.puppetlabs.com/browse/PUP-3829
         if fact('operatingsystemmajrelease').to_i > 6
           shell('ln -s /bin/pip /bin/pip-python')
@@ -16,22 +16,22 @@ describe 'curator class' do
 
     if fact('osfamily') == 'Debian'
       it 'installs pip' do
-        shell("apt-get -y install python-pip")
+        shell('apt-get -y install python-pip')
       end
     end
 
-    it 'should work idempotently with no errors' do
+    it 'works idempotently with no errors' do
       pp = <<-EOS
       class { 'curator': }
       EOS
 
       # Run it twice and test for idempotency
-      apply_manifest(pp, :catch_failures => true)
-      apply_manifest(pp, :catch_changes  => true)
+      apply_manifest(pp, catch_failures: true)
+      apply_manifest(pp, catch_changes: true)
     end
 
     describe command('which curator') do
-      its(:exit_status) { should eq 0 }
+      its(:exit_status) { is_expected.to eq 0 }
     end
   end
 end
